421c0212067da945f452c965a0f125b58faa7876
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2022-01-21 08:58:40 -0300

    criado endpoint put para atualização

--- src/Controller/IncomingController.php
+++ src/Controller/IncomingController.php
@@ -8,7 +8,7 @@ use App\DTO\IncomingDTO;
 use Symfony\Component\HttpFoundation\Request;
 use App\Service\IncomingService;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use App\Entity\Incoming;
 
 #[Route('/receitas')]
 class IncomingController extends AbstractController
@@ -25,12 +25,16 @@ class IncomingController extends AbstractController
     }
 
     #[Route(methods: ['POST'], name: 'incoming_save')]
-    public function save(Request $request): JsonResponse
+    function save(Request $request): JsonResponse
     {
         $payload = json_decode($request->getContent());
-        $dataReceita = \DateTime::createFromFormat('d/m/Y', $payload->data)->format('Y-m-d');
-        $incomingDTO = new IncomingDTO($payload->descricao, $payload->valor, $dataReceita);
-        $incomingSaved = $this->incomingService->save($incomingDTO->converterDTOToEntity());
+        $responseErrors = $this->responseWithErrorsInputNotNull($this->verifyDataIncoming($payload));
+        
+        if ($responseErrors != null) {
+            return $responseErrors;
+        }
+        
+        $incomingSaved = $this->incomingService->save($this->createIncomingWithPayload($payload));
         
         if ($incomingSaved == null) {
             return $this->badRequestBuilder('Já existe uma receita com a descrição registrada nesse mês');
@@ -40,7 +44,7 @@ class IncomingController extends AbstractController
     }
     
     #[Route(methods: ['GET'], name: 'incoming_get_all')]
-    public function getAll():JsonResponse
+    function getAll():JsonResponse
     {
         $incomings = $this->incomingService->getAll();
         $incomingDTOs = IncomingDTO::convertListIncomingToListIncomingDTO($incomings);
@@ -48,7 +52,7 @@ class IncomingController extends AbstractController
     }
     
     #[Route('/{id}', methods: ['GET'], name: 'incoming_find_by_id')]
-    public function findIncomingDetails(int $id): JsonResponse
+    function findIncomingDetails(int $id): JsonResponse
     {
         try {
             $incoming = $this->incomingService->findById($id);
@@ -56,6 +60,64 @@ class IncomingController extends AbstractController
         } catch (\RuntimeException $ex) {
             return $this->badRequestBuilder($ex->getMessage());
         }
+    }
+    
+    #[Route('/{id}', methods: ['PUT'], name: 'incoming_update')]
+    function updateIncomingById(int $id, Request $request): JsonResponse 
+    {
+        $payload = json_decode($request->getContent());
+        $responseErrors = $this->responseWithErrorsInputNotNull($this->verifyDataIncoming($payload));
+        
+        if ($responseErrors != null) {
+            return $responseErrors;
+        }
+        
+        try {
+            $incoming = $this->incomingService->update($id, $this->createIncomingWithPayload($payload));
+            return new JsonResponse($incoming);
+        } catch (\RuntimeException $ex) {
+            return $this->badRequestBuilder($ex->getMessage());
+        }
+    }
+    
+    private function createIncomingWithPayload($payload):Incoming
+    {
+        $this->logger->info('createIncomingWithPayload - payload: '. json_encode($payload));
+        $dataReceita = \DateTime::createFromFormat('d/m/Y', $payload->data)->format('Y-m-d');
+        $incomingDTO = new IncomingDTO($payload->descricao, $payload->valor, $dataReceita);
+        return $incomingDTO->converterDTOToEntity();
+    }
+    
+    private function verifyDataIncoming($payload) 
+    {
+        $errors = array();
+        
+        if (!isset($payload->descricao)):
+            array_push($errors, 'campo descrição não informado');
+        endif;
+        
+        if (!isset($payload->valor)):
+            array_push($errors, 'campo valor não informado');
+        endif;
+        
+        if (!isset($payload->data)):
+            array_push($errors, 'campo data não informado');
+        endif;
+        
+        return $errors;
+    }
+    
+    private function responseWithErrorsInputNotNull($errors):?JsonResponse
+    {
+        if (count($errors) == 0):
+            return null;
+        endif;
+        
+        return new JsonResponse([
+            'status' => 'BAD_REQUEST',
+            'code' => 400,
+            'errors' => $errors
+        ], 400);
     }
     
     private function badRequestBuilder($message): JsonResponse
