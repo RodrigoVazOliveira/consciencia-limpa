cbdec228397cf96376297dae6945a763d831c0ee
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2022-01-29 17:15:15 -0300

    Criar regra para verificar se possui filtro de descrição ou não

--- src/Controller/IncomingController.php
+++ src/Controller/IncomingController.php
@@ -19,7 +19,7 @@ final class IncomingController extends AbstractController
     private LoggerInterface $logger;
     private IncomingService $incomingService;
     private ValidatorInterface $validator;
-    
+
     function __construct(LoggerInterface $logger, IncomingService $incomingService, ValidatorInterface $validator)
     {
         $this->logger = $logger;
@@ -35,21 +35,21 @@ final class IncomingController extends AbstractController
         if ($incoming instanceof JsonResponse) {
             return $incoming;
         }
-        
+
         try {
             return new JsonResponse($this->incomingService->save($incoming), 201);
         } catch (\RuntimeException $ex) {
-            return ErrorExceptions::badRequestBuilder($ex->getMessage()); 
+            return ErrorExceptions::badRequestBuilder($ex->getMessage());
         }
     }
-    
+
     #[Route(methods: ['GET'], name: 'incoming_get_all')]
-    function getAll():JsonResponse
+    function getAll(Request $request): JsonResponse
     {
-        $incomings = $this->incomingService->getAll();
-        return new JsonResponse(IncomingDTO::convertListIncomingToListIncomingDTO($incomings));
+        $incomings = $this->verifyGetAllFilterDescription($request);
+        return new JsonResponse(IncomingDTO::convertListToListDTO($incomings));
     }
-    
+
     #[Route('/{id}', methods: ['GET'], name: 'incoming_find_by_id')]
     function findIncomingDetails(int $id): JsonResponse
     {
@@ -60,9 +60,9 @@ final class IncomingController extends AbstractController
             return ErrorExceptions::badRequestBuilder($ex->getMessage());
         }
     }
-    
+
     #[Route('/{id}', methods: ['PUT'], name: 'incoming_update')]
-    function updateIncomingById(int $id, Request $request): JsonResponse 
+    function updateIncomingById(int $id, Request $request): JsonResponse
     {
         $validationJson = new ValidationJson($this->validator, json_decode($request->getContent()));
         $incomingUpdate = $validationJson->createIncomingWithPayload();
@@ -77,15 +77,24 @@ final class IncomingController extends AbstractController
             return ErrorExceptions::badRequestBuilder($ex->getMessage());
         }
     }
-    
+
     #[Route('/{id}', methods: ['DELETE'], name: 'incoming_delete_by_id')]
     function deleteIncomingById(int $id): JsonResponse
     {
         try {
             $this->incomingService->delete($id);
             return new JsonResponse('', 204);
-        } catch(\RuntimeException $ex) {
+        } catch (\RuntimeException $ex) {
             return ErrorExceptions::badRequestBuilder($ex->getMessage());
         }
+    }
+
+    private function verifyGetAllFilterDescription(Request $request)
+    {
+        if ($request->query->get('descricao') != null) {
+            return $this->incomingService->getByDescription($request->query->get('descricao'));
+        }
+        
+        return $this->incomingService->getAll();
     }
 }
